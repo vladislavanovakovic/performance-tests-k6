c66efcf5c3964bcbdf4c43d2545da4b4b063a394
LEVI9\vl.novakovic <vl.novakovic@levi9.com>
2024-03-27 10:55:21 +0100

    added thresholds

--- 1-script.js
+++ 1-script.js
@@ -1,15 +1,23 @@
-import { sleep } from 'k6';
+import { sleep,check } from 'k6';
 import http from 'k6/http';
 
 export const options = {
   duration: '30s',
   vus: 25,
   thresholds: {
-    http_req_duration: ['p(95)<500'],
+    http_req_duration: ['p(95)<5000'],
   },
 };
 
 export default function () {
-  http.get('http://test.k6.io/contacts.php');
+  let res = http.get('http://test.k6.io/contacts.php');
   sleep(3);
+
+  check (res, {
+    'is status 200': (r) => r.status === 200,
+    'text verification': (r) => r.body.includes("support@k6.io")
+
+  })
+  sleep(3);
+
 }
