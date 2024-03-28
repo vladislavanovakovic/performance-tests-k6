5c52544d759f3bad6932bd2bbbfdf66b863042d9
LEVI9\vl.novakovic <vl.novakovic@levi9.com>
2024-03-28 10:08:49 +0100

    fix

--- 1-script.js
+++ 1-script.js
@@ -3,7 +3,7 @@ import http from 'k6/http';
 
 export const options = {
   duration: '1m',
-  vus: 50,
+  vus: 30,
   thresholds: {
     http_req_duration: ['p(95)<500'],
     http_req_failed: ["rate<0.01"]
