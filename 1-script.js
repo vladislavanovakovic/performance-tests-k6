30c94660f406a9e5b9e97c6276e9a5834e0b4d11
LEVI9\vl.novakovic <vl.novakovic@levi9.com>
2024-04-01 13:37:19 +0200

    number of vus updated

--- 1-script.js
+++ 1-script.js
@@ -3,7 +3,7 @@ import http from 'k6/http';
 
 export const options = {
   duration: '1m',
-  vus: 30,
+  vus: 15,
   thresholds: {
     http_req_duration: ['p(95)<500'],
     http_req_failed: ["rate<0.01"]
