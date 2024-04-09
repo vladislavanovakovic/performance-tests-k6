e4b08a9c229ed12feb4c785af14457b0403656ce
LEVI9\vl.novakovic <vl.novakovic@levi9.com>
2024-04-09 10:02:13 +0200

    update

--- 1-script.js
+++ 1-script.js
@@ -2,21 +2,20 @@ import { sleep,check } from 'k6';
 import http from 'k6/http';
 
 export const options = {
-  duration: '1m',
+  duration: '30s',
   vus: 15,
   thresholds: {
-    http_req_duration: ['p(95)<500'],
+    http_req_duration: ['p(95)<200'],
     http_req_failed: ["rate<0.01"]
   },
 };
 
 export default function () {
-  let res = http.get('https://test.k6.io/news.php');
+  let res = http.get('http://kube.local/');
   sleep(3);
 
   check (res, {
     'is status 200': (r) => r.status === 200,
-    'text verification': (r) => r.body.includes("In the news")
 
   })
   sleep(3);
