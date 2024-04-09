d7dd6c75116524b2afd92c818e47e42b783dd89d
LEVI9\vl.novakovic <vl.novakovic@levi9.com>
2024-04-09 12:46:39 +0200

    updated script

--- 1-script.js
+++ 1-script.js
@@ -1,117 +1,116 @@
-import { sleep,check } from 'k6';
-import http from 'k6/http';
+import { sleep, check } from "k6";
+import http from "k6/http";
 
 export const options = {
   duration: '30s',
-  vus: 5,
+  vus: 1,
   thresholds: {
-    http_req_duration: ['p(95)<20000'],
-    http_req_failed: ["rate<0.01"]
+    http_req_duration: ["p(95)<20000"],
+    http_req_failed: ["rate<0.01"],
   },
 };
 
 export default function () {
 
-  var domain = 'https://test-api.k6.io/'
-  let res = http.get( domain,
-    {
-      tags: {
-        name: 'Homepage',
-      },
+    const credentials = {
+        username: 'test_' + Date.now(),
+        password: 'secret_' + Date.now(),
     }
-  );
 
-  check(res, {
-    'is status 200': (r) => r.status === 200,
-    'text verification': (r) => r.body.includes('Collection of HTTP and WebSocket APIs for experimentation with k6'
-      ),
-  });
-  sleep(1);
+   let res = http.post(
+        'https://test-api.k6.io/user/register/',
+        JSON.stringify(credentials),
+        {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+    );
+    
+    console.log(credentials)
 
-  const credentials = {
-    username: 'test_' + Date.now(),
-    password: 'secret_' + Date.now(),
-}
-   res = http.post(
-  'https://test-api.k6.io/user/register/',
-  JSON.stringify(credentials),
-  {
-      headers: {
-          'Content-Type': 'application/json'
-      }
-  }
-);
-console.log(credentials)
- res = http.post(
-  'https://test-api.k6.io/auth/token/login/',
-  JSON.stringify(
-      {
-          username: credentials.username,
-          password: credentials.password
-      }
-  ),
-  {
-      headers: {
-          'Content-Type': 'application/json'
-      }
-  }
-);
+     res = http.post(
+        'https://test-api.k6.io/auth/token/login/',
+        JSON.stringify(
+            {
+                username: credentials.username,
+                password: credentials.password
+            }
+        ),
+        {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+    );
+   
+    const accessToken = res.json().access;
 
-const accessToken = res.json().access;
 
-http.get(
-  'https://test-api.k6.io/my/crocodiles/',
-  {
-      headers:{
-          Authorization: 'Bearer ' + accessToken
-      }
-  }
-)
-res = http.post(
-  'https://test-api.k6.io/my/crocodiles/',
-  JSON.stringify(
-      {
-          name: "Random croc",
-          sex: "M",
-          date_of_birth: "2023-10-20"
-      }
-  ),
-  {
-      headers:{
-          Authorization: 'Bearer ' + accessToken,
-          'Content-Type': 'application/json'
+    http.get(
+        'https://test-api.k6.io/my/crocodiles/',
+        {
+            headers:{
+                Authorization: 'Bearer ' + accessToken
+            }
+        }
+    );
+    check(res,{
+      'status is 200': (r) => r.status === 200
+  })
 
-      }
-  },
+    res = http.post(
+        'https://test-api.k6.io/my/crocodiles/',
+        JSON.stringify(
+            {
+                name: "Random croc",
+                sex: "M",
+                date_of_birth: "2023-10-20"
+            }
+        ),
+        {
+            headers:{
+                Authorization: 'Bearer ' + accessToken,
+                'Content-Type': 'application/json'
 
-);
-const newCrocodileid = res.json().id;
+            }
+        },
+      
+    );
+    const newCrocodileid = res.json().id;
+    console.log("New crocodile ID:", newCrocodileid);
 
-res = http.get(
-  `https://test-api.k6.io/my/crocodiles/${newCrocodileid}/`,
-  {
-      headers:{
-          Authorization: 'Bearer ' + accessToken
+    res = http.get(
+        `https://test-api.k6.io/my/crocodiles/${newCrocodileid}/`,
+        {
+            headers:{
+                Authorization: 'Bearer ' + accessToken
+            }
+        }
+    );
+    console.log("Requesting crocodile ID:", newCrocodileid);
+    console.log("Response body:", res.body);
+    
+    check(res,{
+        'status is 200': (r) => r.status === 200,
+        'crocodile data': (r) => {
+          const responseData = r.json();
+          return responseData.id === newCrocodileid;
       }
-  }
-);
-check(res,{
-  'status is 200': (r) => r.status === 200,
-  'crocodile id': (r) => r.json().id === newCrocodileid
-});
-console.log(newCrocodileid)
+    });
+    
 
-res = http.del(
-  `https://test-api.k6.io/my/crocodiles/${newCrocodileid}/`,
-  null,
-  {
-      headers:{
-          Authorization: 'Bearer ' + accessToken
-      }
-  }
-);
-check(res, {
-  'is status 204': (r) => r.status === 204
-});
+    res = http.del(
+        `https://test-api.k6.io/my/crocodiles/${newCrocodileid}/`,
+        null,
+        {
+            headers:{
+                Authorization: 'Bearer ' + accessToken
+            }
+        }
+    );
+    check(res,{
+        'status is 204': (r) => r.status === 204
+    })
 
 }
